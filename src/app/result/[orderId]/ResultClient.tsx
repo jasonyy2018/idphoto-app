6a465dbfd3af6342da5562bc8695f588bb3e1a16
jason <290899973@qq.com>
2026-05-03 09:22:17 +0800

    feat: implement ResultClient with download and PDF export functionality for ID photos

--- src/app/result/[orderId]/ResultClient.tsx
+++ src/app/result/[orderId]/ResultClient.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useRef, useState } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import jsPDF from 'jspdf';
